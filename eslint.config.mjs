a526233aabe7b9b31fe04aa284f4888753295574
nomdom149 <nomdom@gmail.com>
2026-06-29 14:35:41 +0200

    Simplify ESLint configuration
    Removed previous ESLint configuration and simplified to an empty rules object.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,8 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals.js";
-import nextTs from "eslint-config-next/typescript.js";
-
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  globalIgnores([
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+/** @type {import('eslint').Linter.Config[]} */
+const eslintConfig = [
+  {
+    rules: {},
+  },
+];
 
 export default eslintConfig;
